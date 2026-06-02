0ab5d08fbd965925f984064a822ff9f6e60ea9d6
supershivas <supershivas@users.noreply.github.com>
2026-06-02 14:18:33 +0200

    Enhance metadata with app description and icons
    Updated metadata to include a description and icons.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 
-export const metadata = { title: 'Idée', description: 'Tes notes' }
+export const metadata = { title: 'Idée', description: 'Home-made note app', icons: {
+    icon: '/favicon-32x32.png',
+    apple: '/favicon-512x512.png',
+  } }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
