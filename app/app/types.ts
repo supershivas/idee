9b1aec437e2c4ed451dd0a46f5b2fa81100352b4
supershivas <supershivas@users.noreply.github.com>
2026-06-05 08:45:11 +0200

    Update types.ts

--- app/app/types.ts
+++ app/app/types.ts
@@ -1,5 +1,4 @@
 export type PageType = 'page' | 'journal'
-
 export type Page = {
   id: string
   parent_id: string | null
@@ -10,11 +9,17 @@ export type Page = {
   favorite?: boolean
   type?: PageType
   position: number
+  created_at: string
   updated_at: string
   deleted_at?: string | null
 }
-
 export function formatSubtitle(date: string) {
+  return new Date(date).toLocaleString('fr-FR', {
+    day: 'numeric', month: 'long', year: 'numeric',
+    hour: '2-digit', minute: '2-digit'
+  })
+}
+export function formatDate(date: string) {
   return new Date(date).toLocaleString('fr-FR', {
     day: 'numeric', month: 'long', year: 'numeric',
     hour: '2-digit', minute: '2-digit'
